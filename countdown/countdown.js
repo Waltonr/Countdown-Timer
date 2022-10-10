c746f279283c61b0d469952707185e85c709f885
Waltonr <waltonr18126@gmail.com>
2022-10-10 14:52:54 -0400

    Fixed Syntax Error in countdown.js File + Added Styling to style.css File + Modified Class Name in index.html File

--- countdown/countdown.js
+++ countdown/countdown.js
@@ -1,18 +1,30 @@
+const dayEl = document.getElementById('day');
+const hourEl = document.getElementById('hour');
+const minEl = document.getElementById('min');
+const secEl = document.getElementById('sec');
+
 const halloween = "31 Oct 2022";
 
 function countdown() {
     const halloweenDate = new Date(halloween);
     const currentDate = new Date();
-
+    
     const totalSeconds = (halloweenDate - currentDate) / 1000;
-
-    const days = math.floor(totalSeconds / 3600 / 24);
-    const hours = math.floor(totalSeconds / 3600) % 24;
-    const minutes = math.floor(totalSeconds / 60) % 60;
-    const seconds = math.floor(totalSeconds) % 60;
+    
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const secs = Math.floor(totalSeconds) % 60;
+    
+    dayEl.innerHTML = days;
+    hourEl.innerHTML = hours;
+    minEl.innerHTML = mins;
+    secEl.innerHTML = secs;
+    
+    console.log(days, hours, mins, secs);
 }
 
 // intial call
 countdown();
 
-setInterval(countdown, 1000)
+setInterval(countdown, 1000);
